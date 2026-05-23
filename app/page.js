6165215081155b3c37d9ad0376e4b36a0fed1920
eherradura <reherradura@gmail.com>
2026-05-22 23:46:10 -0700

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Survey Enforcement Dashboard</h1>
+      <p>Your dashboard is working.</p>
+    </main>
+  );
+}
