9b7d32457c41dbf433a2c404abccff92240644c4
eherradura <reherradura@gmail.com>
2026-05-22 23:57:38 -0700

    Update route.js

--- app/api/jotform/route.js
+++ app/api/jotform/route.js
@@ -1,1 +1,13 @@
 
+export async function GET(request) {
+  return new Response(
+    JSON.stringify({
+      message: "Jotform API route working"
+    }),
+    {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+  );
+}
